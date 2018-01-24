ddccf5b6a45cfd5932a2a35279e16192b7a38636
Hammad Khan <hkhan80@hotmail.com>
2018-01-24 02:19:26 +0100

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # http-server
+
+# Installation via npm:
+
+npm install
+
+This will install http-server so that it may be run from the command line.
+
+# Usage:
+
+node http-server
+
+OR
+
+h=127.0.0.1 p=3000 node server.js
+
+Now you can visit http://localhost:8080 to view your server
+
+# Available Options:
+
+p Port to use (defaults to 8080)
+
+h Address to use (defaults to 0.0.0.0)
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const fs= require('fs');
 const url = require("url");
 const request = require("request");
 const path = require("path");
-const hostname = process.env.HOST || '127.0.0.1';
-const port = process.env.PORT || 3000;
+const hostname = process.env.h || '0.0.0.0';
+const port = process.env.p || 8080;
 
 const server = http.createServer((req, res) => {
 
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
   const serverPath = path.resolve(process.cwd() + '/../');
   const filename = path.join(serverPath, uri);
 
-  fs.exists(filename, function(exists) {
+  fs.exists(filename, (exists) => {
      if (!exists) {
        res.statusCode = 404;
        res.setHeader('Content-Type', 'text/plain');
@@ -20,7 +20,7 @@ const server = http.createServer((req, res) => {
        res.end();
        return;
      }
-     fs.readFile(filename, function(err, data) {
+     fs.readFile(filename, (err, data) => {
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
       res.end();
